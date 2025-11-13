aab762e26cae8bd27263304b24efaf9b42e7a073
Szymon Zych <szymonzych868@gmail.com>
2025-11-13 22:20:36 +0100

    Implement isValid function to check for valid parentheses

--- problems-js/leetcode-solutions/007-valid-parentheses.js
+++ problems-js/leetcode-solutions/007-valid-parentheses.js
@@ -1,1 +1,17 @@
-let isValid = function (s) {};
+let isValid = function (s) {
+  const stack = [];
+  const mapping = { ")": "(", "}": "{", "]": "[" };
+
+  for (const c of s) {
+    if (Object.values(mapping).includes(c)) {
+      stack.push(c);
+    } else if (mapping.hasOwnProperty(c)) {
+      if (stack.pop() !== mapping[c]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+isValid();
