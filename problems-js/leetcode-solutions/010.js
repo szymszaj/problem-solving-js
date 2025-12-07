0fadce4eed139e13bc1fc1b61e66b7d058c35928
Szymon Zych <szymonzych868@gmail.com>
2025-12-07 22:27:59 +0100

    add removeDuplicates function to eliminate duplicates from a sorted array

---
+++ problems-js/leetcode-solutions/010.js
@@ -0,0 +1,14 @@
+let removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let k = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
