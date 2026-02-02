64b6920e6a57618931a675dce09b21906ea452c7
Szymon Zych <szymonzych868@gmail.com>
2026-02-02 22:26:28 +0100

    feat: Add  Two Sum solution

--- problems-js/leetcode-solutions/001-two-sum.js
+++ problems-js/leetcode-solutions/001-two-sum.js
@@ -10,3 +10,14 @@ let counter = createCounter(10);
 counter();
 counter();
 counter();
+
+let twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
