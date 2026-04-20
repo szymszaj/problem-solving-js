0480dcc7581203047197d0b8a3d15c7e8d4a058b
Szymon Zych <szymonzych868@gmail.com>
2026-04-20 22:51:39 +0200

    feat: Implement reduce function to accumulate values in an array

--- problems-js/leetcode-solutions/016-reduce.js
+++ problems-js/leetcode-solutions/016-reduce.js
@@ -1,1 +1,7 @@
-var reduce = function (nums, fn, init) {};
+let reduce = function (nums, fn, init) {
+  let acc = init;
+  for (let i = 0; i < nums.length; i++) {
+    acc = fn(acc, nums[i]);
+  }
+  return acc;
+};
