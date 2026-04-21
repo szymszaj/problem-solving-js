b413508db7a7d61c61f40ecf42bf48e2e4186031
Szymon Zych <szymonzych868@gmail.com>
2026-04-21 23:08:21 +0200

    feat: Add additional examples for reduce function demonstrating product and counts

--- problems-js/leetcode-solutions/016-reduce.js
+++ problems-js/leetcode-solutions/016-reduce.js
@@ -8,3 +8,16 @@ let reduce = function (nums, fn, init) {
 
 const sum = reduce([1, 2, 3, 4, 5], (acc, curr) => acc + curr, 0);
 console.log("Sum:", sum);
+
+const product = reduce([1, 2, 3, 4, 5], (acc, curr) => acc * curr, 1);
+console.log("Product:", product);
+
+const counts = reduce(
+  ["a", "b", "a", "c", "b", "a"],
+  (acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  },
+  {},
+);
+console.log("Counts:", counts);
